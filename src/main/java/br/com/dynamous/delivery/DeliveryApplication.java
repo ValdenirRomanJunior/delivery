76b4644720ffff3952df695d106a88cc46af3da8
Valdenir Roman <vromanjunior@outlook.com>
2021-04-23 11:09:07 -0300

    teste git

--- src/main/java/br/com/dynamous/delivery/DeliveryApplication.java
+++ src/main/java/br/com/dynamous/delivery/DeliveryApplication.java
@@ -111,7 +111,7 @@ public class DeliveryApplication implements CommandLineRunner{
 		Pagamento pgto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
 		ped1.setPagamento(pgto1);
 		
-		Pagamento pgto2 = new PagamentoComBoleto(null,EstadoPagamento.PENDENTE,ped2,sdf.parse("20/10/2018 00:00"),null);
+		Pagamento pgto2 = new PagamentoComBoleto(null,EstadoPagamento.PENDENTE,ped2,sdf.parse("20/10/2019 00:00"),null);
 		ped2.setPagamento(pgto2);
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
