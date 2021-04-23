e7c089ee09dd04e66f255614c7844ce252c449b6
Valdenir Roman <vromanjunior@outlook.com>
2021-04-23 11:37:11 -0300

    endpoint /pedidos/id

--- src/main/java/br/com/dynamous/delivery/domain/Cliente.java
+++ src/main/java/br/com/dynamous/delivery/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.dynamous.delivery.domain.enums.TipoCliente;
@@ -31,6 +32,7 @@ public class Cliente implements Serializable{
 	private String cpfOucnpj;
 	private Integer tipo;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<Pedido>();
 	
